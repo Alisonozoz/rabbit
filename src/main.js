90e1acab49bc6ebf98f06cbf178e55d283dcdd5e
Alison <1964756261@qq.com>
2023-09-21 15:16:16 +0800

    静态资源引入

--- src/main.js
+++ src/main.js
@@ -6,11 +6,7 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-//测试接口函数
-import { getCategory } from './apis/testAPI'
-getCategory().then(res => {
-    console.log(res);
-})
+import '@/styles/common.scss'
 
 const app = createApp(App)
 
