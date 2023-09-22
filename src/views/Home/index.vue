3a87760f6758f78d88c2abfd89ec3778952e7ba3
Alison <1964756261@qq.com>
2023-09-22 17:02:58 +0800

    home-整体结构拆分以及分类的数据渲染

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,4 +1,20 @@
+<script setup>
+import HomeCategory from '@/views/Home/components/HomeCategory.vue'
+import HomeBanner from '@/views/Home/components/HomeBanner.vue'
+import HomeNew from '@/views/Home/components/HomeNew.vue'
+import HomeHot from '@/views/Home/components/HomeHot.vue'
+import HomeProduct from '@/views/Home/components/HomeProduct.vue'
+
+
+</script>
+
 <template>
-    <div>我是home页面</div>
-    <div style="height: 1200px"></div>
+    <div class="container">
+        <HomeCategory></HomeCategory>
+        <HomeBanner></HomeBanner>
+    </div>
+    <HomeNew></HomeNew>
+    <HomeHot></HomeHot>
+    <HomeProduct></HomeProduct>
+
 </template>
